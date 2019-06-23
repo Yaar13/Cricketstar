9426bc5bf317c8181d7b920190a25fe632f363ec
parag <paraggarg66@yahoo.in>
2019-06-23 18:34:50 -0400

    Players and umpires data

--- Cricketstar/main.swift
+++ Cricketstar/main.swift
@@ -8,5 +8,365 @@
 
 import Foundation
 
-print("Hello, World!")
+var player1: Player = Player()
+player1.personId = "1"
+player1.personName = "Sehwag"
+player1.personGender = Gender.Male
+player1.personSponsor = "Sahara"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "India"
+player1.playerJerseyNo = 95
+player1.inPlaying = true
 
+var player2: Player = Player()
+player1.personId = "2"
+player1.personName = "Sachin"
+player1.personGender = Gender.Male
+player1.personSponsor = "Sahara"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "India"
+player1.playerJerseyNo = 94
+player1.inPlaying = true
+
+
+var player3: Player = Player()
+player2.personId = "3"
+player2.personName = "Kohli"
+player2.personGender = Gender.Male
+player2.personSponsor = "Sahara"
+player2.playerType = PlayerType.Batsman
+player2.countryName = "India"
+player2.playerJerseyNo = 96
+player2.inPlaying = true
+
+var player4: Player = Player()
+player1.personId = "4"
+player1.personName = "Dhoni"
+player1.personGender = Gender.Male
+player1.personSponsor = "Sahara"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "India"
+player1.playerJerseyNo = 99
+player1.inPlaying = true
+
+
+var player5: Player = Player()
+player1.personId = "5"
+player1.personName = "Shami"
+player1.personGender = Gender.Male
+player1.personSponsor = "Sahara"
+player1.playerType = PlayerType.Bowler
+player1.countryName = "India"
+player1.playerJerseyNo = 90
+player1.inPlaying = true
+
+var player6: Player = Player()
+player1.personId = "6"
+player1.personName = "Hayden"
+player1.personGender = Gender.Male
+player1.personSponsor = "Alinta energy"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Australia"
+player1.playerJerseyNo = 85
+player1.inPlaying = true
+
+
+var player7: Player = Player()
+player1.personId = "7"
+player1.personName = "Gilchrist"
+player1.personGender = Gender.Male
+player1.personSponsor = "Alinta Energy"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Australia"
+player1.playerJerseyNo = 86
+player1.inPlaying = true
+
+
+var player8: Player = Player()
+player1.personId = "8"
+player1.personName = "Ponting"
+player1.personGender = Gender.Male
+player1.personSponsor = "Alinta Energy"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Australia"
+player1.playerJerseyNo = 87
+player1.inPlaying = true
+
+var player9: Player = Player()
+player1.personId = "9"
+player1.personName = "maxwell"
+player1.personGender = Gender.Male
+player1.personSponsor = "Alinta Energy"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "India"
+player1.playerJerseyNo = 88
+player1.inPlaying = true
+
+
+var player10: Player = Player()
+player1.personId = "10"
+player1.personName = "Brett lee"
+player1.personGender = Gender.Male
+player1.personSponsor = "Alinta Energy"
+player1.playerType = PlayerType.Bowler
+player1.countryName = "Australia"
+player1.playerJerseyNo = 89
+player1.inPlaying = true
+
+var player11: Player = Player()
+player1.personId = "11"
+player1.personName = "Gibbs"
+player1.personGender = Gender.Male
+player1.personSponsor = "Asix"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "South Africa"
+player1.playerJerseyNo = 75
+player1.inPlaying = true
+
+
+var player12: Player = Player()
+player1.personId = "12"
+player1.personName = "dekock"
+player1.personGender = Gender.Male
+player1.personSponsor = "Asix"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "South Africa"
+player1.playerJerseyNo = 76
+player1.inPlaying = true
+
+var player13: Player = Player()
+player1.personId = "13"
+player1.personName = "Amla"
+player1.personGender = Gender.Male
+player1.personSponsor = "Asix"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "South Africa"
+player1.playerJerseyNo = 77
+player1.inPlaying = true
+
+var player14: Player = Player()
+player1.personId = "14"
+player1.personName = "Devilers"
+player1.personGender = Gender.Male
+player1.personSponsor = "Asix"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Australia"
+player1.playerJerseyNo = 78
+player1.inPlaying = true
+
+
+var player15: Player = Player()
+player1.personId = "15"
+player1.personName = "Andrew Nill"
+player1.personGender = Gender.Male
+player1.personSponsor = "Asix"
+player1.playerType = PlayerType.Bowler
+player1.countryName = "Australia"
+player1.playerJerseyNo = 79
+player1.inPlaying = true
+
+
+var player16: Player = Player()
+player1.personId = "16"
+player1.personName = "Farhat"
+player1.personGender = Gender.Male
+player1.personSponsor = "Pepsi"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "PAkistan"
+player1.playerJerseyNo = 65
+player1.inPlaying = true
+
+
+var player17: Player = Player()
+player1.personId = "17"
+player1.personName = "Hameed"
+player1.personGender = Gender.Male
+player1.personSponsor = "Pepsi"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Pakistan"
+player1.playerJerseyNo = 66
+player1.inPlaying = true
+
+var player18: Player = Player()
+player1.personId = "18"
+player1.personName = "Shoib Malik"
+player1.personGender = Gender.Male
+player1.personSponsor = "Pepsi"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Pakistan"
+player1.playerJerseyNo = 67
+player1.inPlaying = true
+
+var player19: Player = Player()
+player1.personId = "19"
+player1.personName = "Sarfaraz"
+player1.personGender = Gender.Male
+player1.personSponsor = "Pepsi"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Pakistan"
+player1.playerJerseyNo = 68
+player1.inPlaying = true
+
+var player20: Player = Player()
+player1.personId = "20"
+player1.personName = "Shoib Akhtar"
+player1.personGender = Gender.Male
+player1.personSponsor = "Pepsi"
+player1.playerType = PlayerType.Bowler
+player1.countryName = "Pakistan"
+player1.playerJerseyNo = 69
+player1.inPlaying = true
+
+var player21: Player = Player()
+player1.personId = "21"
+player1.personName = "Sanath Jayasuriya"
+player1.personGender = Gender.Male
+player1.personSponsor = "Dialog"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Sri Lanka"
+player1.playerJerseyNo = 55
+player1.inPlaying = true
+
+var player22: Player = Player()
+player1.personId = "22"
+player1.personName = "Marvan Attapattu"
+player1.personGender = Gender.Male
+player1.personSponsor = "Dialog"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Sri Lanka"
+player1.playerJerseyNo = 56
+player1.inPlaying = true
+
+var player23: Player = Player()
+player1.personId = "23"
+player1.personName = "Sangakara"
+player1.personGender = Gender.Male
+player1.personSponsor = "Dialog"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Sri Lanka"
+player1.playerJerseyNo = 57
+player1.inPlaying = true
+
+
+var player24: Player = Player()
+player1.personId = "24"
+player1.personName = "Mahila Jayavardne"
+player1.personGender = Gender.Male
+player1.personSponsor = "Dialog"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "Sri Lanka"
+player1.playerJerseyNo = 58
+player1.inPlaying = true
+
+var player25: Player = Player()
+player1.personId = "25"
+player1.personName = "Lasith Malinga"
+player1.personGender = Gender.Male
+player1.personSponsor = "Dialog"
+player1.playerType = PlayerType.Bowler
+player1.countryName = "Sri Lanka"
+player1.playerJerseyNo = 59
+player1.inPlaying = true
+
+
+var player26: Player = Player()
+player1.personId = "26"
+player1.personName = "Marcus Trescothik"
+player1.personGender = Gender.Male
+player1.personSponsor = "New Balance"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "England"
+player1.playerJerseyNo = 45
+player1.inPlaying = true
+
+
+var player27: Player = Player()
+player1.personId = "27"
+player1.personName = "Andrew Strauss"
+player1.personGender = Gender.Male
+player1.personSponsor = "New Balance"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "England"
+player1.playerJerseyNo = 46
+player1.inPlaying = true
+
+var player28: Player = Player()
+player1.personId = "28"
+player1.personName = "Joe Root"
+player1.personGender = Gender.Male
+player1.personSponsor = "New Balance"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "England"
+player1.playerJerseyNo = 47
+player1.inPlaying = true
+
+var player29: Player = Player()
+player1.personId = "29"
+player1.personName = "Buttler"
+player1.personGender = Gender.Male
+player1.personSponsor = "New Balance"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "England"
+player1.playerJerseyNo = 48
+player1.inPlaying = true
+
+var player30: Player = Player()
+player1.personId = "30"
+player1.personName = "Panesar"
+player1.personGender = Gender.Male
+player1.personSponsor = "New Balance"
+player1.playerType = PlayerType.Batsman
+player1.countryName = "England"
+player1.playerJerseyNo = 49
+player1.inPlaying = true
+
+
+
+
+var umpire01: Umpire = Umpire()
+umpire01.personId = "01"
+umpire01.personName = "Alim Dar"
+umpire01.personGender = Gender.Male
+umpire01.personSponsor = "Pakistan"
+umpire01.umpireType = UmpireType.MatchOfficial
+umpire01.numberOfMatches = 224
+umpire01.umpireRank = 1
+
+var umpire02: Umpire = Umpire()
+umpire02.personId = "02"
+umpire02.personName = "David shephard"
+umpire02.personGender = Gender.Male
+umpire02.personSponsor = "England"
+umpire02.umpireType = UmpireType.MatchOfficial
+umpire02.numberOfMatches = 125
+umpire02.umpireRank = 5
+
+
+var umpire03: Umpire = Umpire()
+umpire03.personId = "03"
+umpire03.personName = "Ian Gould"
+umpire03.personGender = Gender.Male
+umpire03.personSponsor = "Newzealand"
+umpire03.umpireType = UmpireType.MatchOfficial
+umpire03.numberOfMatches = 201
+umpire03.umpireRank = 3
+
+
+var umpire04: Umpire = Umpire()
+umpire04.personId = "04"
+umpire04.personName = "kumar Dharmasena"
+umpire04.personGender = Gender.Male
+umpire04.personSponsor = "Sri Lanka"
+umpire04.umpireType = UmpireType.MatchOfficial
+umpire04.numberOfMatches = 120
+umpire04.umpireRank = 4
+
+
+var umpire05: Umpire = Umpire()
+umpire05.personId = "05"
+umpire05.personName = "Nigel Llong"
+umpire05.personGender = Gender.Male
+umpire05.personSponsor = "England"
+umpire05.umpireType = UmpireType.MatchOfficial
+umpire05.numberOfMatches = 190
+umpire05.umpireRank = 2
